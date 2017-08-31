638128963461f4382e7d83cc6e0951004715398b
Yash Pal <yashpalmnd@gmail.com>
2017-08-31 03:26:27 +0000

    Apply fixes from StyleCI

--- src/Support/Database/Schema/Table.php
+++ src/Support/Database/Schema/Table.php
@@ -4,32 +4,35 @@ namespace GitLab\Ripple\Schema;
 
 use GitLab\Ripple\Support\Database\Schema\SchemaManager;
 
-class Table {
-
+class Table
+{
     private $table;
     private $columns;
 
-    public function __construct($table) {
+    public function __construct($table)
+    {
         return $this->table = SchemaManager::schemaTable($table);
     }
 
-    public function table($table) {
-
+    public function table($table)
+    {
         return $this;
     }
 
-    public function columns($columns) {
+    public function columns($columns)
+    {
         dd(\Doctrine\DBAL\Types\Type::getTypesMap());
         foreach ($columns as $column) {
             $this->columns[] = $this->column((object) $column);
         }
     }
 
-    public function column($column) {
+    public function column($column)
+    {
         return (array) [
-                    "name" => $column->name,
-                    "type" => $column->type,
-                    "options" => self::columnOptions($this->columnNullable($this->columnUnsigned((array) $column)))
+                    'name'    => $column->name,
+                    'type'    => $column->type,
+                    'options' => self::columnOptions($this->columnNullable($this->columnUnsigned((array) $column))),
         ];
 //        $this->column = $column;
 //        dd($column['options']);
@@ -37,32 +40,41 @@ class Table {
 //        $this->table->addColumn($column['name'], $column['type'], $column['options']);
     }
 
-    public function create() {
-        $this->table->setPrimaryKey(array("id"));
+    public function create()
+    {
+        $this->table->setPrimaryKey(['id']);
         SchemaManager::databaseManager()->createTable($this->table);
     }
 
-    public function columnNullable($columnArray) {
+    public function columnNullable($columnArray)
+    {
         if (!array_key_exists('nullable', $columnArray)):
             $columnArray['nullable'] = false;
         endif;
+
         return $columnArray;
     }
 
-    public function columnUnsigned($columnArray) {
+    public function columnUnsigned($columnArray)
+    {
         if (!array_key_exists('unsigned', $columnArray)):
             $columnArray['unsigned'] = false;
         endif;
-        return $columnArray;
-    }
-    public function columnAutoIncrement($columnArray) {
-        if (!array_key_exists('unsigned', $columnArray)):
-            $columnArray['unsigned'] = false;
-        endif;
+
         return $columnArray;
     }
 
-    private static function columnOptions($options) {
+    public function columnAutoIncrement($columnArray)
+    {
+        if (!array_key_exists('unsigned', $columnArray)):
+            $columnArray['unsigned'] = false;
+        endif;
+
+        return $columnArray;
+    }
+
+    private static function columnOptions($options)
+    {
         $listOptions = array_diff_key($options, collect($options)->only('name', 'type', 'index')->toArray());
 //        $listOptions = $this->columnNullable($listOptions);
         $columnOptions = (array) [];
@@ -77,5 +89,4 @@ class Table {
 //        dd($columnOptions);
         return $columnOptions;
     }
-
 }
--- src/Traits/DatabaseTables.php
+++ src/Traits/DatabaseTables.php
@@ -4,15 +4,17 @@ namespace GitLab\Ripple\Traits;
 
 use Illuminate\Support\Facades\DB;
 
-trait DatabaseTables {
-
-    private static function tables() {
+trait DatabaseTables
+{
+    private static function tables()
+    {
         $dbTables = DB::select('SHOW TABLES');
         $tables = array_map(function ($table) {
             $get_object_vars = get_object_vars($table);
+
             return reset($get_object_vars);
         }, $dbTables);
+
         return $tables;
     }
-
 }
