cddc468ec7c3d13c763b01dda33e5f42abddf0fd
Yash-Pal <yashpalmnd@gmail.com>
2017-09-17 01:41:27 +0530

    Initialization

--- resources/views/layouts/scripts.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{!! ripple_asset('/js/theme.js') !!}"></script>
-<script src="{!! ripple_asset('/lib/js/tinymce/tinymce.min.js') !!}"></script>
-<script src="{!! ripple_asset('/lib/js/toastr/toastr.min.js') !!}"></script>
-<script src="{!! ripple_asset('/js/ripple.js') !!}"></script>
-@include('Ripple::layouts.toast-notification')
-@stack('page-script')
--- src/Models/Category.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GitLab\Ripple\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Category extends Model {
-    //
-}
