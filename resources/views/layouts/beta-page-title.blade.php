97e5191270b2c304fe6b74c34522eb6987c45ad9
Yash-Pal <yashpalmnd@gmail.com>
2018-02-04 21:20:07 +0530

    ...

--- resources/views/layouts/beta-page-title.blade.php
+++ resources/views/layouts/beta-page-title.blade.php
@@ -1,5 +1,5 @@
 <div class="col">
-    <div class="d-flex align-items-center p-3 mt-3 text-white-50 rounded box-shadow" style="background: #6F42C1">
+    <div class="d-flex align-items-center p-3 mt-3 text-white-50 rounded box-shadow bg-dark">
         <img class="mr-3" src="https://getbootstrap.com/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48">
         <!--<img class="mr-3" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="48" height="48">-->
         <div class="lh-100">
